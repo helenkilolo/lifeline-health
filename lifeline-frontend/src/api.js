e1a4182e293fdf3407a6eb0bbdaef154921b0918
helen kilolo <helenkilolo@gmail.com>
2025-01-07 15:42:48 +0300

    addition

--- lifeline-frontend/src/api.js
+++
@@ -1,6 +0,0 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
-
-export const fetchData = async (endpoint) => {
-  const response = await fetch(`${API_BASE_URL}/${endpoint}`);
-  return response.json();
-};
